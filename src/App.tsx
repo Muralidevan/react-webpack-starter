9646e254a1f98e579ef04abcfce823773e756547
Murali devan <murali@theretailinsights.com>
2021-03-24 23:12:09 +0530

    webpack dev prod build generated

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import LOGO from './logo.svg'
 export const App = () =>{
     return (
         <>
-    <h1> Typescript webpack starter</h1>
+    <h1> Typescript webpack starter mode-{process.env.NODE_ENV} name-{process.env.name}</h1>
     <img src={IMAGE} alt="dark knight" width="300" height="200"/>
     <img src={LOGO} alt="react logo" width="300"/>
 
