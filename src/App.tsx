acc46dd88c0259cea46ebccd4c45629c00e9c7cc
Murali devan <murali@theretailinsights.com>
2021-06-02 18:54:31 +0530

    eslint prettier additonal libs added

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import IMAGE from './dark_knight.jpg'
 import LOGO from './logo.svg'
 import Counter from './Counter'
 
-export const App = () =>{
+export const App = () => {
     return (
         <>
-    <h1> Typescript webpack starter mode-hello{process.env.NODE_ENV} name-{process.env.name}</h1>
-    <img src={IMAGE} alt="dark knight" width="300" height="200"/>
-    <img src={LOGO} alt="react logo" width="300"/>
-    <Counter/>
-
-    </>
-
+            <h1>
+                {' '}
+                Typescript webpack starter mode-hello{process.env.NODE_ENV}{' '}
+                name-
+                {process.env.name}
+            </h1>
+            <img src={IMAGE} alt="dark knight" width="300" height="200" />
+            <img src={LOGO} alt="react logo" width="300" />
+            <Counter />
+        </>
     )
-    
 }
